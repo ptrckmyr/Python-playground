7477901e00229714fc8144f074ca4f74b84561c5
ptrckmyr <pmayer2003@gmail.com>
2022-10-20 19:42:04 +0200

    sda

--- Pythagorean-Triplet.py
+++ Pythagorean-Triplet.py
@@ -1,1 +1,12 @@
-print("Hello World")
+"""Create a function that validates whether three given integers form a Pythagorean triplet.
+The sum of the squares of the two smallest integers must equal the square of the largest number to be validated."""
+
+def is_triplet(x, y, z):
+    xy = (x * x) + (y * y)
+    zz = (z * z)
+    if xy == zz:
+        return True
+    else:
+        return False
+
+print(is_triplet(3, 4, 5))
